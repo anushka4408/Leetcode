c7434e955142d8735d5dc910cc80b6ea4f5810dc
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-01-22 23:39:31 +0530

    Time: 11 ms (59.52%) | Memory: 16 MB (52.81%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -15,6 +15,17 @@ class Solution(object):
                     res[-1][1]=intervals[i][1]
                 elif(res[-1][1]>=intervals[i][1]):
                     continue
-
-            
         return res
+# example 1
+# [[1,3],[2,6],[8,10],[15,18]]
+# res=[1,3]  i=2,6 (3>2)
+# 3>2 and 3<6 res[-1][1]==6 res=[1,6]
+# i=8,10 6<8 res=[1.6 , 8.10]
+# i=15,18 10<15 res=[1.6 ,8.10 ,15.18]
+
+# example2
+# [[1,4] [2,3]]
+# res=[1,4] i=2,3
+# 4>2
+# 4>2 and 4>3 continue res=[1,4]
+
