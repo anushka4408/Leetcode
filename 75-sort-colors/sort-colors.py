9e99d0a8456152ba32fd63f94217a3519c7ff5be
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-01-23 13:15:13 +0530

    Time: 3 ms (24.22%) | Memory: 12.4 MB (35.04%) - LeetSync

---
+++ 75-sort-colors/sort-colors.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        for i in range(len(nums)):
+            swapped=False
+            for j in range(len(nums)-i-1):
+                if(nums[j]>nums[j+1]):
+                    nums[j],nums[j+1]=nums[j+1],nums[j]
+                    swapped=True
+            if not swapped:
+                break
+                
