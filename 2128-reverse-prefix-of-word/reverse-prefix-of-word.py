3f590589e463676f1180a7d796868b6ab4d2b995
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-02-05 23:12:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (7.05%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -5,23 +5,26 @@ class Solution(object):
         :type ch: str
         :rtype: str
         """
-        nword=list(word)
-        ind=-1
-        for i in range(len(nword)):
-            if(nword[i]==ch):
-                ind=i
-                break
+        ind = word.find(ch)  # Find index of first occurrence of ch
+        if ind == -1:
+            return word  # If ch is not found, return the original word
+        return word[:ind+1][::-1] + word[ind+1:]  # Reverse prefix and concatenate
+        # nword=list(word)
+        # ind=-1
+        # for i in range(len(nword)):
+        #     if(nword[i]==ch):
+        #         ind=i
+        #         break
         
-        new=[]
-        for i in range(ind,-1,-1):
-            new.append(nword[i])
-        if(len(new)==0):
-            return word
-        nn=[]
-        for i in range(ind+1,len(nword)):
-            nn.append(nword[i])
-        res=(''.join(new))+(''.join(nn))
-        return res
+        # new=[]
+        # for i in range(ind,-1,-1):
+        #     new.append(nword[i])
+        # if(len(new)==0):
+        #     return word
+        # nn=[]
+        # for i in range(ind+1,len(nword)):
+        #     nn.append(nword[i])
+        # res=(''.join(new))+(''.join(nn))
+        # return res
         
 
-        print(new)
