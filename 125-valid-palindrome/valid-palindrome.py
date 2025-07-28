a6546d1b552866d65aa548cc55b42c351ac23eb1
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-07-28 11:47:10 +0530

    Time: 38 ms (31.49%) | Memory: 53.6 MB (5.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,11 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        def check(left,right):
+            if(left>right):
+                return True
+            if(res[left]!=res[right]):
+                return False
+            return check(left+1,right-1)
+
         new=[]
         for i in s:
             if(i.isalnum()):
                 new.append(i.lower())
         res=''.join(new)
-        if(res==res[::-1]):
-            return True
-        return False
+        return check(0,len(res)-1)
+        
+        # new=[]
+        # for i in s:
+        #     if(i.isalnum()):
+        #         new.append(i.lower())
+        # res=''.join(new)
+        # if(res==res[::-1]):
+        #     return True
+        # return False
