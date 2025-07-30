e4575bb00563ed7dc6fe7f8677c1423e041f024a
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-07-30 10:42:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (12.91%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -7,18 +7,24 @@ class Solution(object):
         :type n: int
         :rtype: None Do not return anything, modify nums1 in-place instead.
         """
-        i=m-1
-        j=n-1
-        k=n+m-1
-        while i>=0 and j>=0:
-            if(nums1[i]>nums2[j]):
-                nums1[k]=nums1[i]
-                i-=1
-            else:
-                nums1[k]=nums2[j]
-                j-=1
-            k-=1
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
+        if(n==0):
+            return nums1
+            # print(nums1)
+        elif(m==0):
+            for i in range(len(nums1)):
+                nums1[i]=nums2[i]
+            # print(nums1)
+
+            return nums1
+        else:
+            i=m
+            while i<len(nums1):
+                for j in range(n):
+                    nums1[i]=nums2[j]
+                    i+=1
+        return nums1.sort()
+        # print(nums1.sort())
+
+
+
+        
