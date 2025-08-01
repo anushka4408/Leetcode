297eedae4e71c73322106d76450923678d11ed36
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-01 22:05:47 +0530

    Time: 3 ms (52.70%) | Memory: 12.7 MB (19.99%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,22 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        #  { ( { } ) }
         stack=[]
-        # {({})}
-        dic={")":"(","}":"{","]":"["}
+        dic={")":'(','}':'{',']':'['}
         for i in s:
             if(i in dic.values()):
                 stack.append(i)
-            if(i in dic):
+            elif(i in dic):
                 if(len(stack)!=0 and stack[-1]==dic[i]):
                     stack.pop()
                 else:
                     return False
-        if(len(stack)==0):
-            return True
-        return False
-
-
-
-
-        
+        return len(stack)==0
