424cbad56ec1969978bc3fe5e998353a602f63cd
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-02 16:56:09 +0530

    Time: 749 ms (42.03%) | Memory: 18.4 MB (62.96%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -4,56 +4,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        nums.sort()  # Sort the array to use the two-pointer technique
-        res = []
-        n = len(nums)
-
+        nums.sort()
+        res=[]
+        n=len(nums)
         for i in range(n):
-        # Skip duplicates for the first number
-            if i > 0 and nums[i] == nums[i - 1]:
+            if(i>0 and nums[i]==nums[i-1]):
                 continue
-        
-            target = -nums[i]
-            left, right = i + 1, n - 1
-
-            while left < right:
-                current_sum = nums[left] + nums[right]
-            
-                if current_sum == target:
-                    res.append([nums[i], nums[left], nums[right]])
-                
-                # Move pointers and skip duplicates
-                    left += 1
-                    right -= 1
-                    while left < right and nums[left] == nums[left - 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right + 1]:
-                        right -= 1
-            
-                elif current_sum < target:
-                    left += 1
+            left=i+1
+            right=n-1
+            while left<right:
+                total=nums[left]+nums[right]+nums[i]
+                if(total==0):
+                    res.append([nums[left],nums[right],nums[i]])
+                    while left<right and nums[left]==nums[left+1]:
+                        left+=1
+                    while left<right and nums[right]==nums[right-1]:
+                        right-=1
+                    left+=1
+                    right-=1
+                elif(total<0):
+                    left+=1
                 else:
-                    right -= 1
-
+                    right-=1
         return res
-
-
-        # nums.sort()
-        # # -1+1+0=0,,-1+1=0,-1-1+2=0,,-1-1=-2
-        # # i=0
-        # # while i<=len(nums):
-        # #     for j in range(i+1,)
-        # k=-1
-        # res=[]
-        # for i in range(len(nums)):
-        #     for j in range(i+1,len(nums)):
-        #         # print(-(nums[i]+nums[j]))
-        #         if(-(nums[i]+nums[j]) in nums):
-        #             k=nums.index(-(nums[i]+nums[j]))
-        #             # print(i,j,k)
-        #             if(i!=j and i!=k and j!=k):
-        #                 res.append([nums[i],nums[j],nums[k]])
-        #             else:
-        #                 continue
-        # return res
-
+            
