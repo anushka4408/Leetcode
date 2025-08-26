c1ae7cf01ba8a272bb013a738a04b573bf00de06
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-27 00:04:20 +0530

    Time: 152 ms (7.42%) | Memory: 19.5 MB (10.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -4,15 +4,38 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        i=0
-        profit=0
-        for j in range(1,len(prices)):
-            if prices[i]<prices[j]:
-                curr=prices[j]-prices[i]
-                profit=max(profit,curr)
-            else:
-                i=j
+
+        #1.
+        # profit=0
+        # for i in range(len(prices)):
+        #     for j in range(i+1,len(prices)):
+        #         if(prices[j]>prices[i]):
+        #             profit=max(profit,prices[j]-prices[i])
+        #         else:
+        #             j+=1
+        # return profit
+
+        #2.
+        # i=0
+        # profit=0
+        # for j in range(1,len(prices)):
+        #     if prices[i]<prices[j]:
+        #         curr=prices[j]-prices[i]
+        #         profit=max(profit,curr)
+        #     else:
+        #         i=j
+        # return profit
+
+        #3.Using recursion
+        #4.Using Memoization
+        #5.Using Tabulation
+        min_price = prices[0]
+        profit = 0
+        for i in range(1, len(prices)):
+            profit = max(profit, prices[i] - min_price)
+            min_price = min(min_price, prices[i])
         return profit
+
             
 
         
