e9ccfb3d526515ed535301e2380f6d26df3ac521
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-29 22:51:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.7 MB (14.83%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -3,7 +3,22 @@ class Solution(object):
         """
         :type prices: List[int]
         :rtype: int
-        """
+        # """
+        # profit=0
+        # for i in range(len(prices)):
+        #     for j in range(i+1,len(prices)):
+        #         if(prices[j]<prices[i]):
+        #             profit+=prices[j]-profit[i]
+
+
+
+
+
+
+
+
+
+
         profit=0
         for j in range(len(prices)-1):
             if(prices[j]<prices[j+1]):
