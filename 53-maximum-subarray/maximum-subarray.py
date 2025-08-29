ec4b5b3083707649015aaf301e41f5113de48159
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-30 00:23:46 +0530

    Time: 232 ms (5.14%) | Memory: 50.5 MB (5.18%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,10 +4,28 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        maxsum=nums[0]
-        currentsum=nums[0]
-        for i in range(1,len(nums)):
-            currentsum=max(nums[i],nums[i]+currentsum)
-            maxsum=max(currentsum,maxsum)
-        return maxsum
+        n = len(nums)
+        dp = [-1] * n
+
+        def f(i):
+            if i == 0:
+                return nums[0]
+            if dp[i] != -1:
+                return dp[i]
+            dp[i] = max(nums[i], nums[i] + f(i-1))
+            return dp[i]
+
+        ans = float('-inf')
+        for i in range(n):
+            ans = max(ans, f(i))
+        return ans
+
+
+
+        # maxsum=nums[0]
+        # currentsum=nums[0]
+        # for i in range(1,len(nums)):
+        #     currentsum=max(nums[i],nums[i]+currentsum)
+        #     maxsum=max(currentsum,maxsum)
+        # return maxsum
             
