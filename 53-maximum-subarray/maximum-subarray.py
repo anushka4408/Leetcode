b3a50cd435bc3931a2b0a247745d8220f0460412
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-08-29 23:35:58 +0530

    Time: 101 ms (15.13%) | Memory: 21.3 MB (45.02%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.py
+++ 53-maximum-subarray/maximum-subarray.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        def maxSubArrayRec(nums, i, curr_sum):
-            if i == len(nums):
-                return float("-inf")   # no subarray
-    
-            # continue subarray or start fresh
-            curr_sum = max(nums[i], curr_sum + nums[i])
-    
-            # best for this position vs later positions
-            return max(curr_sum, maxSubArrayRec(nums, i+1, curr_sum))
-
-        return maxSubArrayRec(nums, 0, 0)
+        maxsum=nums[0]
+        currentsum=nums[0]
+        for i in range(1,len(nums)):
+            currentsum=max(nums[i],nums[i]+currentsum)
+            maxsum=max(currentsum,maxsum)
+        return maxsum
+            
