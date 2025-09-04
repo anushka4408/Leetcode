b58d019f846fd2e50ab1862422e18a6a2197a330
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-09-04 23:44:03 +0530

    Time: 160 ms (47.73%) | Memory: 14.7 MB (58.71%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n=len(word1)
+        m=len(word2)
+        dp=[[0 for i in range(m+1)] for j in range(n+1)]
+        for i in range(1,n+1):
+            for j in range(1,m+1):
+                if(word1[i-1]==word2[j-1]):
+                    dp[i][j]=1+dp[i-1][j-1]
+                else:
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+        lcs=dp[n][m]
+        ans=(n-lcs)+(m-lcs)
+        return ans
