aaee8ae73ec40fa1785e1b3ece1920e64cb8cd1c
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-09-06 01:04:45 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (88.19%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -11,11 +11,33 @@ class Solution(object):
         :type targetSum: int
         :rtype: bool
         """
+
         if not root:
             return False
-        if(not root.left and not root.right):
-            return root.val==targetSum
-        return self.hasPathSum(root.left,targetSum-root.val) or self.hasPathSum(root.right,targetSum-root.val)
+        
+        stack = [(root, root.val)]  # store node and path sum so far
+        
+        while stack:
+            node, s = stack.pop()
+            
+            # Check if leaf and sum matches
+            if not node.left and not node.right and s == targetSum:
+                return True
+            
+            if node.right:
+                stack.append((node.right, s + node.right.val))
+            if node.left:
+                stack.append((node.left, s + node.left.val))
+        
+        return False
+
+
+        # RECURSSION
+        # if not root:
+        #     return False
+        # if(not root.left and not root.right):
+        #     return root.val==targetSum
+        # return self.hasPathSum(root.left,targetSum-root.val) or self.hasPathSum(root.right,targetSum-root.val)
 
                 
 
