bc15fd13507d17fd494a68e1caf027d9cb5ee0c8
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-09-14 00:11:15 +0530

    Time: 337 ms (75.34%) | Memory: 12.5 MB (55.76%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -4,49 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: str
         # """
-        # def ispalindrome(sub):
-        #     if(sub==sub[::-1]):
-        #         return True
-        #     return False
-        # def lps(left,right):
-        #     if left>right:
-        #         return ""
-        #     sub=s[left:right+1]
-        #     if ispalindrome(sub)==True:
-        #         return sub
-        #     leftstr=lps(left+1,right)
-        #     rightstr=lps(left,right-1)
-
-        #     return leftstr if len(leftstr)>=len(rightstr) else rightstr
-
-        # return lps(0,len(s)-1)
-
-
         if not s:
-            return ""
-
-        start, end = 0, 0  # track longest palindrome boundaries
-
-        def expandAroundCenter(left, right):
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
-            return left + 1, right - 1  # valid palindrome boundaries
-
+            return ''
+        start,end=0,0
+        def checkpalindrome(left,right):
+            while left>=0 and right<len(s) and s[left]==s[right]:
+                    left-=1
+                    right+=1
+            return left+1,right-1
         for i in range(len(s)):
-            # Odd length palindrome
-            l1, r1 = expandAroundCenter(i, i)
-            # Even length palindrome
-            l2, r2 = expandAroundCenter(i, i + 1)
+            l1,r1=checkpalindrome(i,i)
+            l2,r2=checkpalindrome(i,i+1)
 
-            # update longest
-            if r1 - l1 > end - start:
-                start, end = l1, r1
-            if r2 - l2 > end - start:
-                start, end = l2, r2
-
+            if(r1-l1>end-start):
+                start,end=l1,r1
+            if(r2-l2>end-start):
+                start,end=l2,r2
         return s[start:end+1]
-
 
         
         
