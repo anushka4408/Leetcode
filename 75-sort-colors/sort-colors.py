ff84e8f3a284d6bf7a17863417b0bf7712879411
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-09-20 18:52:41 +0530

    Time: 7 ms (8.44%) | Memory: 12.5 MB (11.43%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,46 +5,37 @@ class Solution(object):
         :type nums: List[int]
         :rtype: None Do not return anything, modify nums in-place instead.
         """
-
         # SELECTION SORT
         # for i in range(len(nums)):
-        #     miniind=i
+        #     minidx=i
         #     for j in range(i+1,len(nums)):
-        #         if(nums[j]<nums[miniind]):
-        #             miniind=j
-        #     nums[i],nums[miniind]=nums[miniind],nums[i]
+        #         if(nums[j]<nums[minidx]):
+        #             minidx=j
+        #     nums[minidx],nums[i]=nums[i],nums[minidx]
         # return nums
 
         # BUBBLE SORT
-        # for i in range(len(nums)):
-        #     for j in range(0,len(nums)-1-i):
-        #         if(nums[j]>nums[j+1]):
-        #             nums[j],nums[j+1]=nums[j+1],nums[j]
-        # return nums
+        for i in range(len(nums)):
+            for j in range(0,len(nums)-i-1):
+                if(nums[j]>nums[j+1]):
+                    nums[j],nums[j+1]=nums[j+1],nums[j]       
+        return nums
+# Bubble sort is stable (doesn’t change order of equal elements).
 
-        # INSERTION SORT
-        # for i in range(1,len(nums)):
-        #     key=nums[i]
-        #     j=i-1
-        #     while j>=0 and nums[j]>key:
-        #         nums[j+1]=nums[j]
-        #         j-=1
-        #     nums[j+1]=key
-        # return nums
-        return nums.sort()
+# Time Complexity: O(n²)
+
+# Space Complexity: O(1) (in-place sorting)
+
+    # INSERTION SORT
+        for i in range(1,len(nums)):
+            key=nums[i]
+            j=i-1
+            while j>0 and nums[j]>nums[key]:
+                nums[j+1]=nums[j]
+                j-=1
+            nums[j+1]=key
+        return nums
+
 
             
-
-
-
-
-
-        # for i in range(len(nums)):
-        #     # swapped=False
-        #     for j in range(len(nums)-i-1):
-        #         if(nums[j]>nums[j+1]):
-        #             nums[j],nums[j+1]=nums[j+1],nums[j]
-        #             # swapped=True
-        #     # if not swapped:
-        #     #     break
                 
