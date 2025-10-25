a16484de59d1ddd8420471d8f6303f6e6aaa7134
Anushka Singh <137861013+anushka4408@users.noreply.github.com>
2025-10-25 23:19:08 +0530

    Time: 32 ms (88.41%) | Memory: 19.9 MB (80.13%) - LeetSync

--- 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1445-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -6,14 +6,24 @@ class Solution(object):
         :type threshold: int
         :rtype: int
         """
+        # count=0
+        # window=arr[0:k]
+        # if(sum(window)//k>=threshold):
+        #     count+=1
+        # for i in range(k,len(arr)):
+        #     window.append(arr[i])
+        #     window.pop(0)
+        #     if(sum(window)//k>=threshold):
+        #         count+=1
+        # return count
         count=0
-        window=arr[0:k]
-        if(sum(window)//k>=threshold):
+        windowsum=sum(arr[0:k])
+        if(windowsum//k>=threshold):
             count+=1
         for i in range(k,len(arr)):
-            window.append(arr[i])
-            window.pop(0)
-            if(sum(window)//k>=threshold):
+            windowsum+=arr[i]
+            windowsum-=arr[i-k]
+            if(windowsum//k>=threshold):
                 count+=1
         return count
             
